e271cdd533d6374edde7cf0fcb4ff3198cf11228
Alex Brelsford <abrelsfo@utk.edu>
2016-02-28 21:26:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 'use strict';
 module.exports = function extendString(){
 
-  String.prototype.strip = function (delimiter) {
+  String.prototype.strip = function (delimiter, strip) {
     const argLength = arguments.length;
-    if (argLength > 1) {
-      throw new Error('Strip expected 1 argument, got '+ argLength);
+    if (strip === undefined){
+      strip = true;
+    }
+
+    if (argLength !== 1 && argLength !== 2) {
+      throw new Error('Strip expected 1 or 2 arguments, got '+ argLength);
     }
 
     if (typeof delimiter !== 'string') {
       throw new TypeError('Strip expected delimiter to be a string, got ' + (typeof delimiter));
     }
 
+    if (typeof strip !== 'boolean') {
+      throw new TypeError('Strip expected strip to be a number, got ' + (typeof strip));
+    }
+
     const len = this.length;
     if (len === 0) {
-      throw new Error('Expected input string length >= 1, got ' + this.length);
+        throw new Error('Expected input string length >= 1, got ' + this.length);
     } else {
-      var re = new RegExp(delimiter, "g");
+      if (strip){
+        var re = new RegExp(delimiter, "gi");
+      } else {
+        var re = new RegExp(delimiter, "g");
+      }
       return this.replace(re, '');
     }
   };
